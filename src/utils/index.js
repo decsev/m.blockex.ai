604cf750ffbc836c1c03fdcbc142e64af9ef1dd6
decsev <261754000@qq.com>
2018-03-18 23:14:08 +0800

    修改配置

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,10 @@
 import config from './config';
 import Func from './function.js';
+import request from './request';
 
 export {
     config,
     Func,
+    request,
 };
 
-export * from './request';
